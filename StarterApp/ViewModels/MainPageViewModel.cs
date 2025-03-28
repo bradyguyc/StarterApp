cfacc4eea42b825e4f6b8208ea51a76defe0c314
Brady Guy Chambers <81129882+bradyguyc@users.noreply.github.com>
2025-03-28 11:06:58 -0500

    Enhance authentication flow and UI in the app
    - Updated `MainPage.xaml` to include error popup, sign-in/out buttons, and a `ListView` for token claims.
    - Added new activities in `AndroidManifest.xml` for Microsoft Identity Client authentication.
    - Modified `MainActivity.cs` to configure MSAL parameters and handle authentication results.
    - Updated `StarterApp.csproj` with new embedded resources and package references for MSAL.
    - Enhanced `MainPageViewModel.cs` with user authentication management and claims updating.
    - Introduced `appsettings.json` for Azure AD configuration settings.
    - Added `updatemsal.ps1` script to automate updates for `MsalActivity.cs` and `AndroidManifest.xml`.
    - Created new classes in `StarterApp.MSALClient` for MSAL configuration and functionality.
    - Implemented `PublicClientSingleton` for managing MSAL client instance and token acquisition.
    - Added utility classes like `WindowsHelper` for platform-specific operations.
    - Updated `MsalActivity.cs` to include necessary namespaces for authentication handling.

--- StarterApp/ViewModels/MainPageViewModel.cs
+++ StarterApp/ViewModels/MainPageViewModel.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 
-using StarterApp.Models;
-
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
+using Microsoft.Identity.Client;
+
+using StarterApp.Models;
+using StarterApp.MSALClient;
 
 namespace StarterApp.ViewModels
 {
     public partial class MainPageViewModel : ObservableObject
     {
         [ObservableProperty] ShowPopUpDetails popupDetails;
+        [ObservableProperty] private bool isSignedIn = false;
+        [ObservableProperty] private List<string> idTokenClaims = new();
+
+        [ObservableProperty] private bool showErrorPopUp = false;
+        [ObservableProperty] private bool errorSuccess = false;
+        [ObservableProperty] private ShowPopUpDetails errorDetails = new ShowPopUpDetails();
         public MainPageViewModel()
         {
             PopupDetails = new ShowPopUpDetails();
-            PopupDetails.IsOpen = false;
+
+            IsSignedIn = false;
+            ErrorDetails.IsOpen = false;
+            IAccount cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
+            if (cachedUserAccount != null)
+            {
+                IsSignedIn = true;
+                UpdateClaims();
+            }
         }
         [RelayCommand]
         void TestShowError()
@@ -30,6 +47,65 @@ namespace StarterApp.ViewModels
         void ClosePopUp()
         {
             PopupDetails.IsOpen = false;
+        }
+
+        private async Task UpdateClaims()
+        {
+            _ = await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
+
+            var claims = PublicClientSingleton.Instance.MSALClientHelper.AuthResult.ClaimsPrincipal.Claims.Select(c => c.Value);
+
+            IdTokenClaims = claims.ToList();
+        }
+        [RelayCommand]
+        public Task SignIn()
+        {
+            IAccount? cachedUserAccount = null; ;
+            cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
+
+            return MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                if (cachedUserAccount == null)
+                {
+                    string token = null;
+                    try
+                    {
+                        token = await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        PopupDetails.IsOpen = true;
+                        PopupDetails.ErrorCode = "ERR-001";
+                        PopupDetails.ErrorMessage = ex.Message;
+                        //Console.WriteLine(ex.Message);
+                    }
+                    if ((token == null) && (ErrorDetails.IsOpen == false))
+                    {
+                        PopupDetails.IsOpen = true;
+                        PopupDetails.ErrorCode = "ERR-001";
+                        //todo: display error message to user that they need to sign in and the sign process was exited before completing sign in
+                    }
+                    else
+                    {
+                        IAccount cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
+                        if (cachedUserAccount != null)
+                        {
+                            IsSignedIn = true;
+                            await UpdateClaims();
+                        }
+                    }
+                }
+            });
+        }
+
+        [RelayCommand]
+        public Task SignOut()
+        {
+            return MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                await PublicClientSingleton.Instance.MSALClientHelper.SignOutUserAsync();
+                IsSignedIn = false;
+            });
         }
     }
 }
