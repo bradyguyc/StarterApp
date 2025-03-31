0d3c532e465e60b4445eccf26fe2cbd08608bd49
Brady Guy Chambers <81129882+bradyguyc@users.noreply.github.com>
2025-03-31 14:12:44 -0500

    Update UI, MSAL configuration, and project dependencies
    - Added `InvertedBoolConverter` and replaced `SfPopup` with `ErrorPopupView` in `MainPage.xaml`.
    - Updated `AndroidManifest.xml` to reflect new MSAL scheme.
    - Configured MSAL parameters in `AppDelegate.cs` and initialized the client app.
    - Upgraded project to target .NET 10.0 and added application ID properties in `StarterApp.csproj`.
    - Updated project dependencies, including `DevExpress.Maui.Core` and new versions of MSAL packages.
    - Modified `MainPageViewModel.cs` to include logging and refined error handling.
    - Changed `ClientId` in `appsettings.json` for Azure AD.
    - Added `Entitlements.plist` for keychain access groups.

--- StarterApp/Platforms/iOS/AppDelegate.cs
+++ StarterApp/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,8 @@
 ﻿using Foundation;
+using StarterApp.MSALClient;
+using Microsoft.Identity.Client;
+
+using UIKit;
 
 namespace StarterApp
 {
@@ -6,5 +10,16 @@ namespace StarterApp
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // configure platform specific params
+            PlatformConfig.Instance.RedirectUri = $"msal{PublicClientSingleton.Instance.MSALClientHelper.AzureAdConfig.ClientId}://auth";
+
+            // Initialize MSAL and platformConfig is set
+            IAccount existinguser = Task.Run(async () => await PublicClientSingleton.Instance.MSALClientHelper.InitializePublicClientAppAsync()).Result;
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
     }
 }
